8f06dba9b2ee1021cc5513d393d2fce4edad90ae
Arundhati Apte <97678771+ArundhatiApte@users.noreply.github.com>
2022-03-25 13:58:07 +0300

    added creating package script

---
+++ createPackage.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+zip -r TorControls.package.zip . \
+  -x node_modules/\*\
+  -x doc/\*\
+  -x .git/\*\
+  -x examples/\*\
+  -x *test\*\
+  -x ".gitignore"\
+  -x *test/\*\
+  -x "createPackage.sh"
